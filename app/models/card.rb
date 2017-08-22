1eb7f8e4789a89d83ff5a0a81fdd4794227939fa
mirakurum <mirakuru.m@yandex.ru>
2017-08-22 13:57:48 +0400

    validation

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,22 @@
+class EqualValidator < ActiveModel::Validator
+  def validate(record)
+    if record.original_text == record.translated_text
+      record.errors[:base] << "Поля не должны совпадать"
+    end
+  end
+end
+
 class Card < ApplicationRecord
+  validates :translated_text, :review_date, presence: true
+  validates :original_text, presence: true, uniqueness: true
+  validates_with EqualValidator
+
+  before_validation :capitalize_fields_and_create_review_date
+
+  private
+    def capitalize_fields_and_create_review_date
+      self.original_text = self.original_text.strip.capitalize
+      self.translated_text = self.translated_text.strip.capitalize
+      self.review_date = Time.now + 3.days
+    end
 end
