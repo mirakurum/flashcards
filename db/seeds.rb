4e3547908e3553592eec1bf2b2d33ce4aee65ce8
mirakurum <mirakuru.m@yandex.ru>
2017-08-28 16:47:46 +0400

    houndci style fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ doc = Nokogiri::HTML(open(
   "http://1000mostcommonwords.com/1000-most-common-russian-words/"))
 
 doc.xpath('//tr').each_with_index do |link, index|
-a = Nokogiri::HTML(link.to_s)
+  a = Nokogiri::HTML(link.to_s)
   if index.positive?
     Card.create(original_text: a.css('td')[1].content,
                 translated_text: a.css('td')[2].content)
